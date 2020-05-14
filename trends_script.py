2b980d09fe54e6d14489b90e0c25dbfa2f8c5c09
tomashege <41945595+tomashege@users.noreply.github.com>
2020-05-14 08:57:22 +0200

    scraping

---
+++ trends_script.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+import pandas as pd
+import time
+#you have to change this for every computer...
+driver = webdriver.Chrome('/Users/tomashegewisch/Downloads/chromedriver')
+the_trends = []
+def get_trend_pre_day(date):
+    country = "south-africa"
+    #date = "2020-02-01"
+    #time_international = "18"
+    the_trends_on_day = []
+    for time_international in range(0,23):
+#         if time_international == 15 and date == "2020-03-24":
+#             continue
+        url = "https://getdaytrends.com/"+country+"/"+date+"/"+str(time_international)+"/"
+        global driver
+        driver.get(url)
+        #time.sleep(2)
+        try:
+            clicker = driver.find_elements_by_xpath('//*[@id="trends"]/div/a')[0]
+            clicker.click()
+            a = driver.find_elements_by_xpath('//*[@id="trends"]/table/tbody/tr/td/a')
+            print(country+" "+str(date)+" "+str(time_international))
+            for i in a:
+                the_trends.append(i.text)
+                #the_trends_on_day.append(i.text)
+        except:
+            print("The trends on this day and time is not Available")
+            driver = webdriver.Chrome('/Users/tomashegewisch/Downloads/chromedriver')
+    #the_trends.append(the_trends_without_duplicates)
+from datetime import date, timedelta
+start_date = date(2020, 3, 19)
+end_date = date(2020, 5, 20)
+
+#start_date = date(2020, 3, 24)
+#end_date = date(2020, 3, 24)
+delta = timedelta(days=1)
+while start_date <= end_date:
+    #print (start_date.strftime("%Y-%m-%d"))
+    get_trend_pre_day(start_date.strftime("%Y-%m-%d"))
+    start_date += delta
+        
+#Remove The duplicates
+print("Before duplicates: "+ str(len(the_trends)))
+#the_trends = list(dict.fromkeys(the_trends))
+the_trends_without_duplicates = [] 
+[the_trends_without_duplicates.append(x) for x in the_trends if x not in the_trends_without_duplicates]
+print("After duplicates are removed: " + str(len(the_trends_without_duplicates)))
+
+#Create CSV File
+
+out = "Trends,\n"
+#create the document
+for i in the_trends_without_duplicates:
+    out+= str(i)+",\n"
+#write the documnet
+with open("trends.csv", "w")as text_file:
+    text_file.write(out) 
+print("file saved") 
