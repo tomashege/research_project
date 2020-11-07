5cfc7ae63f48d79e4fdb4ea8ec8b09103f47c47c
tomashege <41945595+tomashege@users.noreply.github.com>
2020-11-07 14:21:00 +0200

    cleaning up

--- Tomas/get_tweets.R
+++
@@ -1,6 +0,0 @@
-library(rtweet)
-library(tidyverse)
-temp <- search_tweets("immediate effect",retryonratelimit = T)
-df <- temp
-#write_csv (df,"test.csv")
-write_as_csv(temp, "tweet_data.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
